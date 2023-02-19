023c6d878525ba92f4ab660f51a494a1f75dda2a
Gabriel Casselman <gabriel@Gabriels-MacBook-Pro.local>
2023-02-18 17:11:13 -0800

    Added full screen

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+// #cgo pkg-config: gtk+-3.0
+// #include <gtk/gtk.h>
+// void fullscreen(void *win)
+// {
+// 	gtk_window_fullscreen(GTK_WINDOW(win));
+// }
+import "C"
 import (
 	"github.com/webview/webview"
 )
@@ -12,6 +19,9 @@ func main() {
 	defer w.Destroy()
 	w.SetTitle("piTimer")
 	w.SetSize(480, 320, webview.HintNone)
+
+	// Use custom C function to fullscreen
+	C.fullscreen(w.Window())
 
 	w.Navigate("https://google.com")
 	w.Run()
